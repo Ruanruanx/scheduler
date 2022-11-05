9deaf843fc7d0ec2bfaa1f3a3fe94516cbdb6cc2
Ruanruanx <zxin7@uwo.ca>
2022-11-05 17:42:03 -0400

    Adde onSubmit and modified onSave

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -3,11 +3,11 @@ import Button from "components/Button";
 import InterviewerList from "components/InterviewerList"
 
 export default function Form(props) {
-    const [student, setStudent] = useState(props.student || "");
-    const [interviewer, setInterviewer] = useState(props.interviewer || null);
+    let [student, setStudent] = useState(props.student || "");
+    let [interviewer, setInterviewer] = useState(props.interviewer || null);
     
     function reset(){
-        setStudent="";
+        setStudent("");
         setInterviewer(null);
     }
 
@@ -19,7 +19,7 @@ export default function Form(props) {
     return (
         <main className="appointment__card appointment__card--create">
             <section className="appointment__card-left">
-                <form autoComplete="off">
+                <form autoComplete="off" onSubmit={e=>{e.preventDefault();}} >
                     <input
                         className="appointment__create-input text--semi-bold"
                         name="name"
@@ -38,7 +38,7 @@ export default function Form(props) {
             <section className="appointment__card-right">
                 <section className="appointment__actions">
                     <Button danger onClick={()=>cancel()}>Cancel</Button>
-                    <Button confirm onClick={props.onSave} >Save</Button>
+                    <Button confirm onClick={()=>props.onSave(student,interviewer)} >Save</Button>
                 </section>
             </section>
         </main>
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,5 +1,9 @@
-import React from "react"
-import "components/Appointment/styles.scss"
+import React from "react";
+import "components/Appointment/styles.scss";
+import Empty from "./Empty";
+import Show from "./Show";
+import Header from "./header";
+
 export default function Appointment(props){
     return(
         <article className="appointment"></article>
