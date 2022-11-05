94c5f1d6e0121cf7940d5fd46157ddd89f8dcff1
Ruanruanx <zxin7@uwo.ca>
2022-11-05 05:53:28 -0400

    confirm

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,8 @@ import Appointment from "components/Appointment/index";
 import Header from "components/Appointment/header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
+
 
 storiesOf("Button", module)
   .addParameters({
@@ -154,5 +156,9 @@ storiesOf("Appointment", module)
     interviewer={interviewer}
     onEdit={action("onEdit")}
     onDelete={action("onDelete")}
-  
+  />)
+  .add("Confirm",()=><Confirm 
+    message="Delete the appointment?"
+    onConfirm={action("onConfirm")}
+    onCancel={action("onCancel")}
   />)
