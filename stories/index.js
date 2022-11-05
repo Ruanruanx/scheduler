fb33c10962032142640979e03e792b68132b4628
Ruanruanx <zxin7@uwo.ca>
2022-11-05 08:20:49 -0400

    Added components in appointment

--- stories/index.js
+++ stories/index.js
@@ -15,6 +15,10 @@ import Header from "components/Appointment/header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
+
 
 
 storiesOf("Button", module)
@@ -160,5 +164,24 @@ storiesOf("Appointment", module)
   .add("Confirm",()=><Confirm 
     message="Delete the appointment?"
     onConfirm={action("onConfirm")}
+    onCancel={action("onCancel")}
+  />)
+  .add("Status",()=><Status
+    message="Deleting"
+  />)
+  .add("Error",()=><Error
+    message="Could not delete appointment"
+    onClose={action("onClose")}
+  />)
+  .add("Edit",()=><Form
+    student={"Joey"}
+    interviewer={1}
+    interviewers={interviewers}
+    onSave={action("onSave")}
+    onCancel={action("onCancel")}
+  />)
+  .add("Create",()=><Form
+    interviewers={interviewers}
+    onSave={action("onSave")}
     onCancel={action("onCancel")}
   />)
