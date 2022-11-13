790fdd852e5ffaf7edbc0595f435961ce7a00357
Ruanruanx <zxin7@uwo.ca>
2022-11-13 06:57:25 -0500

    getInterview test

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,22 +1,35 @@
 export function getAppointmentsForDay(state, day) {
-    let result=[];
+    let result = [];
     for (const date of state.days) {
-        if (date.name===day) {
-            for(let i of date.appointments){
+        if (date.name === day) {
+            for (let i of date.appointments) {
                 result.push(state.appointments[i])
             }
-            
+
         }
     }
     return result;
 }
 
-export function getInterview(state,interview){
-    if(!interview){
+export function getInterview(state, interview) {
+    if (!interview) {
         return null;
     }
-    return{
-        student:interview.student,
-        interviewer:state.interviewers[interview.interviewer]
+    return {
+        student: interview.student,
+        interviewer: state.interviewers[interview.interviewer]
     }
+}
+
+export function getInterviewersForDay(state,day) {
+    let result = [];
+    for (const date of state.days) {
+        if (date.name === day) {
+            for (let i of date.appointments) {
+                result.push(state.interviewers[i])
+            }
+
+        }
+    }
+    return result;
 }
