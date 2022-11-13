010ad33e15152b540d6f25eaa362457c94dc4e99
Ruanruanx <zxin7@uwo.ca>
2022-11-13 05:22:52 -0500

    replace test

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,25 +1,29 @@
-import React, {useCallback, useState} from "react";
+import React, { useCallback, useState } from "react";
 
-export default function useVisualMode(initialMode){
-    const [mode,setMode]=useState(initialMode);
-    const [history,setHistory]=useState([initialMode]);
-    const transition=(mode)=>{
-        setMode(mode);
-        setHistory(prev=>([...prev,mode]))
-        return mode;
+export default function useVisualMode(initialMode) {
+    const [mode, setMode] = useState(initialMode);
+    const [history, setHistory] = useState([initialMode]);
+    const transition = (mode, replace = false) => {
+        if (replace) {
+            setHistory(prev => ([...prev.slice(0, prev.length - 1), mode]))
+            setMode(mode);
+        } else {
+            setMode(mode);
+            setHistory(prev => ([...prev, mode]))
+        }
     }
-    const back=(()=>{
-        if(history.length<2){
+    const back = (() => {
+        if (history.length < 2) {
             return history;
         }
-        setHistory(prev=>(
-            prev.slice(0,prev.length-1))
-            );
-    
-        const lastMode = history[history.length-2];
+        setHistory(prev => (
+            prev.slice(0, prev.length - 1))
+        );
+
+        const lastMode = history[history.length - 2];
         setMode(lastMode)
 
         return mode;
     })
-    return {mode,transition,back};
+    return { mode, transition, back };
 }
